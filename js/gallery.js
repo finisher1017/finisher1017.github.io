17bc8b4a1e11a025cd1eea351af698a2928cf360
Jonathan Seubert <finisher1017@gmail.com>
2019-01-11 20:56:29 -0500

    add image optimization 1

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,10 @@ function activateGallery() {
 	
 
 	thumbnails.forEach(function(thumbnail) {
+		// Preload large images.
+		let newImageSrc = thumbnail.dataset.largeVersion;
+		let largeVersion = new Image();
+		largeVersion.src = newImageSrc;
 		thumbnail.addEventListener("click", function() {
 			// Set clicked image as main  image.
 			let newImageSrc = thumbnail.dataset.largeVersion;
